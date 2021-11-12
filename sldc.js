0357a2bf7cb75bc3a072b917b393337cd55f8cbf
Molly Rusk <mollyrusk@gmail.com>
2021-11-12 09:39:58 -0700

    sldc.js done

--- sldc.js
+++ sldc.js
@@ -12,8 +12,20 @@
 // Planning
 // Testing & Integration
 
+var devLifecycle = [];
+
+devLifecycle.push('Planning', 'Analysis of Requirements','Design', 'Implementation', 'Testing & Integration', 'Maintain');
+
+console.log(devLifecycle);
 
 //////////////////PROBLEM 2////////////////////
 /*
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
 */
+
+let AnalysisOfRequirements = 'Thoughful planning of how to solve the problem with goals and outcomes in mind';
+let Design = 'Creating a design that fits the proposed architecture';
+let Implementation = '';
+let Maintain = 'The software is checked for performance issues';
+let Planning = 'A solution is made based on the design decision';
+let TestingAndIntegration = 'The software is checked for performance and quality';
